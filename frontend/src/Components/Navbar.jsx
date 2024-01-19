317c5ec43d5cc0fad854f2eebd01f135089344e2
Paras <paraskamaliya5@gmail.com>
2024-01-19 16:36:16 +0530

    done draft 1

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from "../Assets/logo.png";
 const Navbar = () => {
-    return <div className="flex justify-between items-center bg-Navbar border border-NavbarBorder px-4 py-8 px-4 py-8 ml-10 mr-10 mt-10 mb-0 rounded-2xl bg-gradient-to-r from-right to-left h-22 gap-2 sticky top-10 z-100 backdrop-filter backdrop-blur-md">
+    return <div className="flex justify-between items-center bg-Navbar border border-NavbarBorder px-4 py-8 ml-10 mr-10 mt-10 mb-0 rounded-2xl bg-gradient-to-r from-right to-left h-22 gap-2 sticky top-10 z-100 backdrop-filter backdrop-blur-md">
         <div className="w-[50%]">
             <img src={logo} alt="logo" className="h-14" />
         </div>
