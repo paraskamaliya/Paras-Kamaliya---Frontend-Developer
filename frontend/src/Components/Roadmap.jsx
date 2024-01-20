e917582e9aeb0f31d694ff1a7fe994e626e64de4
Paras <paraskamaliya5@gmail.com>
2024-01-20 15:44:34 +0530

    update in Slide

--- frontend/src/Components/Roadmap.jsx
+++ frontend/src/Components/Roadmap.jsx
@@ -5,8 +5,8 @@ const Roadmap = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="177" height="20" viewBox="0 0 177 20" fill="none" className="absolute left-[8%] z-1">
                 <path d="M0.551003 18.3865C48.1951 6.09829 115.064 -0.428688 176.712 2.97441" stroke="#515151" stroke-width="3" />
             </svg>
-            <div className="grid grid-flow-col scroll-auto gap-[5%] overflow-y-auto overflow-x-visible mt-[5%] scroll-container">
-                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start">
+            <div className="flex overflow-x-scroll overflow-y-auto cursor-grab active:cursor-grabbing gap-[5%] mt-[5%] max-w-[100%]">
+                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start min-w-[33.33%] grab-scroll-item">
                     <h2 className="text-2xl w-fit m-auto bg-bitCoinColor text-black font-bold p-3">PHASE-1</h2>
                     <div className="ml-[10%] mr-[10%] mb-[10%] p-[10%] border rounded-2xl border-bitCoinColor">
                         <h2 className="font-medium text-2xl">Initial Design and Development</h2>
@@ -14,7 +14,7 @@ const Roadmap = () => {
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
                     </div>
                 </div>
-                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start">
+                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start min-w-[33.33%] grab-scroll-item">
                     <h2 className="text-2xl w-fit m-auto bg-bitCoinColor text-black font-bold p-3">PHASE-2</h2>
                     <div className="ml-[10%] mr-[10%] mb-[10%] p-[10%] border rounded-2xl border-bitCoinColor">
                         <h2 className="font-medium text-2xl">Release and Testing of Bita Market & Bridge</h2>
@@ -22,7 +22,7 @@ const Roadmap = () => {
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
                     </div>
                 </div>
-                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start">
+                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start min-w-[33.33%] grab-scroll-item">
                     <h2 className="text-2xl w-fit m-auto bg-bitCoinColor text-black font-bold p-3">PHASE-3</h2>
                     <div className="ml-[10%] mr-[10%] mb-[10%] p-[10%] border rounded-2xl border-bitCoinColor">
                         <h2 className="font-medium text-2xl">Bitapad & <br /> Rollout</h2>
@@ -30,7 +30,7 @@ const Roadmap = () => {
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
                     </div>
                 </div>
-                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start">
+                <div className="text-center border border-black rounded-2xl bg-black scroll-snap-align-start min-w-[33.33%] grab-scroll-item">
                     <h2 className="text-2xl w-fit m-auto bg-bitCoinColor text-black font-bold p-3">PHASE-4</h2>
                     <div className="ml-[10%] mr-[10%] mb-[10%] p-[10%] border rounded-2xl border-bitCoinColor">
                         <h2 className="font-medium text-2xl">Launch of <br /> NakaLend</h2>
@@ -40,6 +40,6 @@ const Roadmap = () => {
                 </div>
             </div>
         </div>
-    </div>
+    </div >
 }
 export default Roadmap;
